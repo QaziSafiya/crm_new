f059cafde6192b0a9196320dc8769c2be88de42e
Uditkishore <uditkishorofficial@gmail.com>
2023-08-17 11:14:13 +0530

    modified reducer & store to store data locally.

--- src/store/StoreProvider.jsx
+++ src/store/StoreProvider.jsx
@@ -2,41 +2,63 @@ import { useReducer } from "react";
 import reducer from "./reducer.js";
 import { StoreContext } from "./store-context.js";
 
+function getStoragedItem(key, storageType = localStorage) {
+    try {
+      const value = storageType.getItem(key);
+      return value ? JSON.parse(value) : null;
+    } catch (error) {
+      console.error(`Error getting session storage item: ${error.message}`);
+      return null;
+    }
+}
+  
+  const storedAuth = getStoragedItem("AUTH_USER", sessionStorage);
+  const storedUserDetails = getStoragedItem("SET_USER_DETAILS");
+  const storedLibPdfDoc = getStoragedItem("LIB_PDF_DOC");
+  const storedGstLogin = getStoragedItem("GST_LOGIN");
+  const storedGstMonth = getStoragedItem("SET_GST_MONTH");
+  const storedGstQuater = getStoragedItem("SET_GST_QUARTER");
+  const storedGstYear = getStoragedItem("SET_GST_YEAR");
+  const storedDashboardData = getStoragedItem("SET_DASHBOARD_DATA");
+  const storedPDF_DOC = getStoragedItem("PDF_DOC");
+  const storedGSTR_MODE = getStoragedItem("GSTR_MODE");
+  const storedGSTR_OBJ = getStoragedItem("GSTR_OBJ");
+  
 export default function StoreProvider({ children }) {
     const initialState = {
         auth: {
-            currentUser: null,
-            token: null,
+            currentUser: storedAuth ? storedAuth.user : null,
+            token: storedAuth ? storedAuth.token : null,
             pending: true,
         },
-        user: {},
-        libraryPdfDoc:null,
+        user: storedUserDetails ? storedUserDetails : {},
+        libraryPdfDoc: storedLibPdfDoc? storedLibPdfDoc : null,
         gst: {
-            party_name: '',
+            party_name: storedGstLogin? storedGstLogin.party_name : '',
             isLoggedIn: false,
-            gstin: null,
-            username: null,
-            month: null,
-            quarter: 1,
-            year: new Date().getFullYear()
+            gstin:storedGstLogin ? storedGstLogin.gstin : null,
+            username: storedGstLogin? storedGstLogin.username : null,
+            month: storedGstMonth? storedGstMonth : null,
+            quarter: storedGstQuater? storedGstQuater : 1,
+            year: storedGstYear? storedGstYear : new Date().getFullYear()
         },
         dashboard: {
-            data: {},
+            data: storedDashboardData? storedDashboardData : {},
             loading: true,
         },
         sidebarOpen: true,
         pdfDoc: {
-            intro:'',
-            businessName: "",
-            pnmList: [],
-            owner: '',
+            intro: storedPDF_DOC? storedPDF_DOC.intro : '',
+            businessName: storedPDF_DOC? storedPDF_DOC.businessName:"",
+            pnmList: storedPDF_DOC? storedPDF_DOC.pnmList :[],
+            owner: storedPDF_DOC? storedPDF_DOC.owner : '',
             rented: false,
             loan: false,
-            data: {},
+            data: storedPDF_DOC? storedPDF_DOC.data : {},
         },
         gstr: {
-            mode: "offline",
-            gstrObj: [{}],
+            mode: storedGSTR_MODE? storedGSTR_MODE : "offline",
+            gstrObj: storedGSTR_OBJ? storedGSTR_OBJ : [{}],
         }
     };
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,18 @@
-import { AUTH_FROM_REDIRECT, AUTH_USER, CLEAR_REDIRECT, CLOSE_SIDEBAR, GST_LOGIN, LOGOUT, OPEN_SIDEBAR, SET_DASHBOARD_DATA, SET_GST_MONTH, SET_GST_YEAR, SET_USER_DETAILS, TOGGLE_SIDEBAR, UPDATE_USER , PDF_DOC, SET_GST_QUARTER, SET_REDIRECT, GSTR_MODE, GSTR_OBJ, PDF_DOC_INVOICE, LIB_PDF_DOC} from "./actions.js";
+import { AUTH_FROM_REDIRECT, AUTH_USER, CLEAR_REDIRECT, CLOSE_SIDEBAR, GST_LOGIN, LOGOUT, OPEN_SIDEBAR, SET_DASHBOARD_DATA, SET_GST_MONTH, SET_GST_YEAR, SET_USER_DETAILS, TOGGLE_SIDEBAR, UPDATE_USER, PDF_DOC, SET_GST_QUARTER, SET_REDIRECT, GSTR_MODE, GSTR_OBJ, PDF_DOC_INVOICE, LIB_PDF_DOC } from "./actions.js";
+
+function setStorageItem(key, value, storageType = localStorage) {
+    try {
+        storageType.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(`Error setting session storage item: ${error.message}`);
+    }
+}
 
 export default function reducer(state, { type, payload }) {
     console.log(type, payload);
     switch (type) {
         case AUTH_USER:
+            setStorageItem('AUTH_USER', payload, sessionStorage);
             return { ...state, auth: { ...state.auth, currentUser: payload.user, token: payload.token, pending: false } };
         case AUTH_FROM_REDIRECT:
             return { ...state, auth: { ...state.auth, currentUser: payload.user, token: payload.token, pending: false, redirect: payload.redirect } };
@@ -14,6 +23,18 @@ export default function reducer(state, { type, payload }) {
         case UPDATE_USER:
             return { ...state, auth: { ...state.auth, currentUser: { ...state.auth.currentUser, ...payload } } };
         case LOGOUT:
+            sessionStorage.removeItem('AUTH_USER');
+            localStorage.removeItem('SET_DASHBOARD_DATA');
+            localStorage.removeItem('SET_USER_DETAILS');
+            localStorage.removeItem('GST_LOGIN');
+            localStorage.removeItem('SET_GST_MONTH');
+            localStorage.removeItem('SET_GST_QUARTER');
+            localStorage.removeItem('SET_GST_YEAR');
+            localStorage.removeItem('LIB_PDF_DOC');
+            localStorage.removeItem('PDF_DOC');
+            localStorage.removeItem('PDF_DOC');
+            localStorage.removeItem('GSTR_MODE');
+            localStorage.removeItem('GSTR_OBJ');
             return { ...state, auth: { ...state.auth, currentUser: null, token: null } };
         case TOGGLE_SIDEBAR:
             return { ...state, sidebarOpen: !state.sidebarOpen };
@@ -22,10 +43,13 @@ export default function reducer(state, { type, payload }) {
         case CLOSE_SIDEBAR:
             return { ...state, sidebarOpen: false };
         case SET_DASHBOARD_DATA:
+            setStorageItem('SET_DASHBOARD_DATA', payload);
             return { ...state, dashboard: { ...state.dashboard, data: payload, loading: false } };
         case SET_USER_DETAILS:
+            setStorageItem('SET_USER_DETAILS', payload);
             return { ...state, user: payload };
-        case GST_LOGIN:
+            case GST_LOGIN:
+            setStorageItem('GST_LOGIN', payload);
             return {
                 ...state,
                 gst: {
@@ -37,17 +61,22 @@ export default function reducer(state, { type, payload }) {
                 },
             };
         case SET_GST_MONTH:
+            setStorageItem('SET_GST_MONTH', payload);
             return { ...state, gst: { ...state.gst, month: payload }, };
         case SET_GST_QUARTER:
+            setStorageItem('SET_GST_QUARTER', payload);
             return { ...state, gst: { ...state.gst, quarter: payload } }
         case SET_GST_YEAR:
+            setStorageItem('SET_GST_YEAR', payload);
             return { ...state, gst: { ...state.gst, year: payload }, };
-            case LIB_PDF_DOC:
+        case LIB_PDF_DOC:
+            setStorageItem('LIB_PDF_DOC', payload);
       return {
         ...state,
         libraryPdfDoc: payload,
       };
         case PDF_DOC:
+            setStorageItem('PDF_DOC', payload);
             return {
                 ...state,
                 pdfDoc: {
@@ -61,9 +90,11 @@ export default function reducer(state, { type, payload }) {
                 },
             };
         case GSTR_MODE:
+            setStorageItem('GSTR_MODE', payload);
             return { ...state, gstr:{ mode: payload }};
 
         case GSTR_OBJ: 
+        setStorageItem('GSTR_OBJ', payload);
             return {...state, gstr: {...state.gstr, gstrObj: payload}}
             case PDF_DOC_INVOICE:
       return {
